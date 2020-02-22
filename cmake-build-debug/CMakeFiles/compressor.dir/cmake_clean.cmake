8c4e2576ba3496e5031adc4cdb3bac7e3d5169bd
Fraks51 <semzhuv@yandex.ru>
2020-02-23 00:11:05 +0300

    Init commit

---
+++ cmake-build-debug/CMakeFiles/compressor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compressor.dir/main.cpp.o"
+  "compressor"
+  "compressor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/compressor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
